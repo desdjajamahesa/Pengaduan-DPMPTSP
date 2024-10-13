3847303f985b4814a902f398c80e9ad1fe1a6d7b
Des Djaja Mahesa <108280558+desdjajamahesa@users.noreply.github.com>
2024-10-14 01:35:38 +0700

    develop UI login v.1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,10 +7,15 @@
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>Document</title>
+    <title>S!Padu</title>
+    <style>
+        body{
+            background-image: url('bg-textured.png');
+        }
+    </style>
 </head>
 
-<body>
+<body class="bg-blue-50 flex items-center justify-center min-h-screen">
     <!-- Tailwind CSS setup -->
     <!-- Uncomment and configure Tailwind CSS if needed -->
     <!--
@@ -18,51 +23,47 @@
     <body class="h-full">
     -->
 
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="w-full max-w-md p-8 bg-blue-200 rounded-lg shadow-xl">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
-                account</h2>
+            <img class="mx-auto h-14 w-auto" src="sipadu.png" alt="SiPadu DPMPTSP Kota Cimahi">
+            <h2 class="mt-7 text-center text-2xl font-bold leading-9 tracking-tight text-slate-600">Masukkan Akun</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="{{ route('login') }}" method="POST">
                 @csrf
                 <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
-                        address</label>
+                    <label for="email" class="block text-sm px-1 font-semibold leading-6 text-slate-700">Email</label>
                     <div class="mt-2">
                         <input id="email" name="email" type="email" autocomplete="email" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            class="block w-full h-10 rounded-xl shadow-md border-0 px-3.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-orange-300 sm:text-md sm:leading-6">
                     </div>
                 </div>
 
                 <div>
                     <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        <label for="password" class="block text-sm px-1 font-semibold leading-6 text-slate-700">Password</label>
                         <div class="text-sm">
                             <a href="{{ route('password.request') }}"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                                class="font-semibold text-cyan-600 hover:text-cyan-400">Lupa password?</a>
                         </div>
                     </div>
                     <div class="mt-2">
                         <input id="password" name="password" type="password" autocomplete="current-password" required
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            class="block w-full h-10 rounded-xl shadow-md border-0 px-3.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-orange-300 sm:text-md sm:leading-6">
                     </div>
                 </div>
 
                 <div>
                     <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                        in</button>
+                        class="flex w-full justify-center rounded-xl bg-cyan-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-md hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600">Masuk</button>
                 </div>
             </form>
 
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
+            <p class="mt-10 text-center text-sm text-gray-700">
+                Belum punya akun?
                 <a href="{{ route('register') }}"
-                    class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register</a>
+                    class="font-semibold leading-6 text-cyan-700 hover:text-cyan-500">Daftar Akun</a>
             </p>
         </div>
     </div>
