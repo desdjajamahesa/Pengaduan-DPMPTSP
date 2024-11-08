20712e6916c009095e799c649191796ddc2ad0f4
Des Djaja Mahesa <108280558+desdjajamahesa@users.noreply.github.com>
2024-11-09 01:26:48 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ use App\Http\Controllers\StatistikController;
 Route::get('login', function () {
     return view('login');
 })->name('login');
-Route::post('login', [AuthController::class, 'login'])->name('login.submit')->middleware('auth');
+Route::post('login', [AuthController::class, 'login'])->name('login.submit');
 
 // Halaman register dan proses register
-Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register')->middleware('auth');
-Route::post('register', [RegisterController::class, 'register'])->middleware('auth');
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
 
 // Halaman reset password
 Route::get('password/reset', function () {
@@ -41,37 +41,25 @@ Route::get('/home', function () {
     return view('user.home');
 })->name('sop.index')->middleware('auth');
 
-Route::get('/home/sop', [SopController::class, 'show'])->name('sop.index')->middleware('auth');;
+Route::get('/home/sop', [SopController::class, 'show'])->name('sop.index')->middleware('auth');
 
 Route::get('/detail', function () {
     return view('user.detail');
 })->name('detail')->middleware('auth');
 
 // View User
-Route::get('/pengaduan/{id}', [PengaduanController::class, 'show'])->name('pengaduan.show')->middleware('auth');;
+Route::get('/pengaduan/{id}', [PengaduanController::class, 'show'])->name('pengaduan.show')->middleware('auth');
 
 // Form pengaduan (tanpa middleware)
-Route::patch('/home', [PengaduanController::class, 'create'])->name('pengaduan.form')->middleware('auth');;
+Route::patch('/home', [PengaduanController::class, 'create'])->name('pengaduan.form')->middleware('auth');
 Route::put('/pengaduan/{id}', [PengaduanController::class, 'update'])->name('pengaduan.update')->middleware('auth');
-Route::patch('/home', [PengaduanController::class, 'create'])->name('pengaduan.index')->middleware('auth');;
+Route::patch('/home', [PengaduanController::class, 'create'])->name('pengaduan.index')->middleware('auth');
+
+
 // -----------------------------
 // Superadmin Routes
 // -----------------------------
 Route::get('/home', [StatistikController::class, 'index'])->name('home')->middleware('auth');
-
-
-
-// Halaman pengaduan superadmin
-
-// Halaman kontak management superadmin
-
-// Halaman Data Admin superadmin
-// Route::get('/kontaksuper', [ContactOptionController::class, 'SuperAdminindex'])->name('contacts.SuperAdminindex');
-// Route::post('/kontaksuper', [ContactOptionController::class, 'update'])->name('contacts.update');
-
-// Route::get('/adminsuper', [DashboardController::class, 'admin'])->name('superadmin.admin');
-// Route::get('/usersuper', [DashboardAController::class, 'users'])->name('superadmin.user');
-
 
 // Route Pelaporan Admin dan Super Admin
 Route::get('/pelaporan', [PelaporanController::class, 'index'])->name('pelaporan');
