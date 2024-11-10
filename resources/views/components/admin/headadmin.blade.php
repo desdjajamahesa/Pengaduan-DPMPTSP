53a0b18c69df835a5aeedcf992ab278ee6a43a08
smerull <smaasmerul@gmail.com>
2024-11-10 14:24:05 +0700

    navbar

--- resources/views/components/admin/headadmin.blade.php
+++ resources/views/components/admin/headadmin.blade.php
@@ -1,7 +1,10 @@
 <div class="flex-1 flex flex-col">
     <!-- Header -->
-    <header class="bg-white shadow-md p-4 flex justify-between items-center border-b border-gray-200">
-        <h1 class="text-2xl font-semibold text-gray-800"></h1>
+    <header
+        class="bg-gradient-to-r from-blue-500 to-indigo-600 shadow-md p-4 flex justify-between items-center border-b border-gray-200">
+        <!-- Logo and Title -->
         <div class="flex items-center space-x-4">
-            <button </div>
+            <h1 class="text-4xl font-bold text-white tracking-wider">S!Padu - Sistem Pengaduan</h1>
+        </div>
+
     </header>
